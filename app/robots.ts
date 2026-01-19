83eb7d8d32c96ea6338c99382183dd93eea088f0
phawaaaz <phawaazakin@gmail.com>
2026-01-19 00:32:48 -0800

    Improving the Robot.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://asolbs.com/sitemap.xml',
+sitemap: 'https://www.asolsolution.com/sitemap.xml',
   }
 }
